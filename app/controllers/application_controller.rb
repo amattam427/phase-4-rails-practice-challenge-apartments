9027513b794fe5b0d4e66c381ee70a86a61ce3d0
Ashley Mattam <amattam427@gmail.com>
2022-03-03 20:24:06 -0500

    completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response 
+
+
+private
+
+def render_not_found_response(error)
+    render json: {error: "#{error.model} not found"}, status: :not_found 
+end
+
+def render_invalid_response(invalid)
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+end
+
 end
